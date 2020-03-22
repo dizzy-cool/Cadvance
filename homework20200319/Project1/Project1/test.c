a21015f7592a2f5a652d1aca0f43455df099035b
dizzy-cool <965136370@qq.com>
2020-03-22 23:55:47 +0800

    注意bubbleSort函数中,bound正数,i倒数ls

---
+++ homework20200319/Project1/Project1/test.c
@@ -0,0 +1,129 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <string.h>
+
+int Atoi(const char *str) {
+	assert(str != NULL);
+	char ret = 0;
+	while (*str >= '0' && *str <= '9') {
+		ret = ret * 10 + (*str - '0');
+		str++;
+	}
+	return ret;
+}
+char* Strncat(char* destion, const char* source, size_t num) {
+	assert(destion != NULL && source != NULL);
+	int i = 0;
+	size_t count = 0;
+	while (destion[i] != '\0') {
+		i++;
+
+	}
+	for (count = 0; count < num; count++) {
+		destion[i + count] = source[count];
+		}
+	destion[i + count] = '\0';
+	return destion;
+}
+char* Strncpy(char* destion, const char* source, size_t num) {
+	assert(destion != NULL && source != NULL);
+	size_t i = 0;
+	for (i = 0; i < num; i++) {
+		destion[i] = source[i];
+	}
+	destion[i] = '\0';
+	return destion;
+}
+void BubbleSort(int* arr, int num) {
+	int bound = 0;
+	int i = 0;
+	//[0 bound)   是已经排序的区间
+	//[bound num) 是未排序的区间
+
+	for (bound = 0; bound < num; bound++) {
+		for (i = num - 1; i > bound; i--) {
+			if (arr[i] < arr[i - 1]) {
+				int temp = arr[i];
+				arr[i] = arr[i - 1];
+				arr[i - 1] = temp;
+			}
+		}
+	}
+}
+
+
+int* findSingleDog(int* arr,int num,int* res) {
+
+	BubbleSort(arr, num);
+	int count = 0;
+	int i = 0;
+	
+	if (arr[i] != arr[i + 1]) {
+		res[count] = arr[i]; 
+		count++;
+	}
+	if (arr[num - 1] != arr[num - 2]) {
+		res[count] = arr[num-1];
+		count++;
+	}
+	if (count >= 2) {
+		return res;
+	}
+	
+	i = 1;
+	while (i < num - 1 && count < 2) {
+		if (arr[i] != arr[i - 1] && arr[i] != arr[i + 1]) {
+			res[count] = arr[i];
+			count++;
+		}
+		i++;
+	}
+	return res;
+	
+}
+
+int main(void) {
+	/*char str[100] = " ";
+	while (1) {
+		printf("请输入含数字的字符串:\n");
+		scanf("%s", str);
+		int n = Atoi(str);
+		printf("%d\n", n);
+	}*/
+
+	//char str1[20];
+	//char str2[20];
+	//strcpy(str1, "To be ");
+	//strcpy(str2, "or not to be");
+	//Strncat(str1, str2, 14);
+	//printf("%s\n", str1);
+
+	//char str1[] = "To be or not to be";
+	//char str2[40];
+	//char str3[40];
+
+	///* copy to sized buffer (overflow safe): */
+	//Strncpy(str2, str1, sizeof(str2));
+
+	///* partial copy (only 5 chars): */
+	//Strncpy(str3, str2, 5);
+	//str3[5] = '\0';   /* null character manually added */
+
+	//puts(str1);
+	//puts(str2);
+	//puts(str3);
+
+
+	int arr[] = { 1, 3, 4, 1, 3, 5 };
+	int res[2] = { 0 };
+	findSingleDog(arr, sizeof(arr) / sizeof(arr[0]), res);
+	//BubbleSort(arr, sizeof(arr) / sizeof(arr[0]));
+	for (int i = 0; i < sizeof(arr) / sizeof(arr[0]); i++) {
+		printf("%d\t", arr[i]);
+	}
+	printf("%d %d", res[0], res[1]);
+	system("pause");
+	return 0;
+}
